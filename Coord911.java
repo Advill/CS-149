dec49adf9f496be0b17d8e88866300ff3456b87e
Advil <mcgloinms@gmail.com>
2016-09-07 13:19:41 -0400

    added project 1
    PA01

---
+++ Coord911.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Coord911 {
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		System.out.println("What is the first name of the caller?");
+		String fname = s.nextLine();
+		System.out.println("What is the last name of the caller?");
+		String lname = s.nextLine();
+		System.out.println("What is the preferred name of the caller?");
+		String prefname = s.nextLine();
+		System.out.println("What is the location of the Incident?");
+		String location = s.nextLine();
+		System.out.println("What is the emergency code of the Incident?");
+		String code = s.nextLine();
+		System.out.println("What is the age of the caller?");
+		String age = s.nextLine();
+		System.out.println("What is the x-coordinate of the caller?");
+		double x = s.nextDouble();
+		System.out.println("What is the y-coordinate of the caller?");
+		double y = s.nextDouble();
+		System.out.println(fname + lname + "\nDate: 09/09/2016 \n" + 
+							"age: " + age + "\nNickname: " + prefname +
+							"\nStreet Adress: " + location + "\nCODE\tX	\tY" + 
+							"----\t-\t-\n" + location + "\t" + x + "\t" + y +
+							"\nBy land distance: " + (x + y) + "\nHelicopter Distance"
+							+ (Math.sqrt(x*x + y*y)));
+	}
+}
